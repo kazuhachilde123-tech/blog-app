7c810380974f9c9761a4c37b657301e41f51ec05
Adhim <kazuha.childe123@gmail.com>
2026-06-22 10:33:18 +0700

    fix: app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if(config('app.env') == 'production'){
-            \URL::forceSchema('https')
+            \URL::forceSchema('https');
         }
     }
 }
